73c86fc4122f377447a8781266bbe02e689b31bc
i2gun <i2gun@bk.ru>
2023-12-11 16:36:46 +0600

    Adding last changes

--- task2.py
+++ task2.py
@@ -1,4 +1,44 @@
 import json
+import re
+import datetime
+
+
+def check_date(pat, txt):
+    try:
+        datetime.datetime.strptime(txt, pat)
+    except:
+        return False
+    return True
+
+def check_func(type, pat, txt):
+    if type == 1:
+        print("11")
+        return re.fullmatch(re.compile(pat), txt)
+    elif type == 2:
+        return check_date(pat, txt)
+
+
+def check_pattern(check_type, pat, msg, check_for_quit, item_num):
+    keep_looping = True
+    out = list() if item_num > 1 else ""
+    print(type(out))
+    show_msg = msg + (", либо 'q' для выхода: " if check_for_quit else ": ")
+    while keep_looping and item_num > 0:
+        text = input(show_msg)
+        if check_func(check_type, pat, text):
+            if type(out) is list:
+                print("22")
+                out.append(text)
+            else:
+                print("33")
+                out = text
+            item_num -= 1
+        else:
+            if check_for_quit and text == "q":
+                keep_looping = False
+            else:
+                print("Введено неверное значение")
+    return out
 
 
 def load() -> set:
@@ -19,13 +59,27 @@ def load() -> set:
 def add():
     global phonebook
 
-    phones = list()
-    fullname = input("Введите полное имя: ")
-    numPhones = int(input("Введите количество телефонных номеров: "))
-    for i in range(numPhones):
-        phones.append(input("Введите номер телефона: "))
-    birthday = input("Введите дату родения: ")
-    email = input("Введите адрес электронной почты: ")
+    pattern = r"[А-ЯЁ][а-яё]+\s+[А-ЯЁ][а-яё]+(?:\s+[А-ЯЁ][а-яё]+)?"
+    message = "Введите полное имя"
+    fullname = check_pattern(1, pattern, message, False, 1)
+
+    pattern = r"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$"
+    message = "Введите правильный номер телефона"
+    phones = check_pattern(1, pattern, message, True, 10)
+
+    pattern = "%m/%d/%y"
+    message = "Введите дату родения"
+    birthday = check_pattern(2, pattern, message, True, 1)
+
+    # pattern = r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"	
+    # pattern = r"^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$"
+    pattern = r"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$"
+    
+    message = "Введите адрес электронной почты"
+    email = check_pattern(1, pattern, message, True, 1)
+
+    print("3")
+    
     phonebook[fullname] = {'phones': phones }
     if birthday != "":
         phonebook[fullname]['birthday'] = birthday
@@ -67,9 +121,13 @@ commands = {"add": add, "save": save, "show": show, "search": search, "change": 
 print("Вам доступны следующие команды: ", *commands)
 
 while loopGoOn:
-    command = input("Введите команду: ")
+    print("-----------------------")
+    show()
+    print("-----------------------")
+    command = input("Введите новую команду: ")
     try:
         commands[command]()
+        show()
     except:
         print("Неверная комманда")
 
