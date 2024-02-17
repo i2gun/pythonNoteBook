1ff55dd1ded1f21932b14eeebaf3bcb99a9fa069
i2gun <i2gun@bk.ru>
2024-02-17 13:47:54 +0600

    Refactoring the project for intermadiate attestation

--- check_functions.py
+++ check_functions.py
@@ -9,20 +9,13 @@ def check_date(pattern: re, text: str) -> bool:
         return False
     return True
 
-
-def check_func(check_type: int, pattern: re, text: str) -> bool:
-    if check_type == 1:
-        return re.fullmatch(re.compile(pattern), text)
-    elif check_type == 2:
-        return check_date(pattern, text)
-
-
 def check_pattern(check_type: int, pattern: re, msg: str, check_for_quit: bool, item_num: int) -> tuple[set, str]:
     keep_looping = True
     out = set() if item_num > 1 else ""
     while keep_looping and item_num > 0:
         text = input(msg + (", либо 'q' для выхода: " if check_for_quit else ": "))
-        if check_func(check_type, pattern, text):
+
+        if check_date(pattern, text):
             if type(out) is set:
                 if text in set:
                     print("Такая запись уже вводилась !")
--- command_fucntions.py
+++ command_fucntions.py
@@ -3,81 +3,60 @@ from check_functions import check_pattern
 
 
 def load() -> dict:
-    json_contacts = {"Дядя Ваня": {'phones': [8311654654, 89654515], 'birthday': "05.05.1990", 'email': "12@ya.ru"},
-                     "Дядя Вася": {'phones': [54654541]}}
+    json_records = {'first record': {'text': "Hello World!", 'date': "17.02.2024"},
+                    'some record': {'text': "some note",  'date': "17.02.2024"}, }
     try:
-        with open("contacts.json", "r", encoding="utf-8") as fh:
-            json_contacts = json.loads(fh.read())
+        with open("notes.json", "r", encoding="utf-8") as fh:
+            json_records = json.loads(fh.read())
     except:
-        print("Загрузка тестового телефонного справочника")
+        print("Загрузка заметок из файла")
 
-    return json_contacts
+    return json_records
 
 
-def add(phonebook: dict) -> dict:
-    pattern = (r"^([А-ЯA-Z]|[А-ЯA-Z][\x27а-яa-z]{1,}|[А-ЯA-Z][\x27а-яa-z]{1,}\-([А-ЯA-Z][\x27а-яa-z]{1,}|" +
-               "(оглы)|(кызы)))\040[А-ЯA-Z][\x27а-яa-z]{1,}(\040[А-ЯA-Z][\x27а-яa-z]{1,})?$")
-    fullname = check_pattern(1, pattern, "Введите полное имя", False, 1)
-    for key, el in phonebook:
-        if key == fullname:
-            print("Запись с таким именем уже имеется в справочнике !")
+def add(notebook: dict) -> dict:
+    title = input("Введите заголовок заметки: ")
+    for key, el in notebook:
+        if key.lowercase == title.lowercase:
+            print("Запись с таким заголовком уже имеется в заметках !")
             print("Добавление данных отменено")
             return
 
-    pattern = r"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$"
-    phones = check_pattern(1, pattern, "Введите номер телефона", True, 10)
-    for el in phonebook:
-        if len(set(el["phones"] & phones)) > 0:
-            print("Один из телофоных номеров уже имеется в справочникес !")
-            print("Добавление данных отменено")
-            return
+    phones = input("Введите текст заметки: ")
 
-    pattern = "%m/%d/%y"
-    birthday = check_pattern(2, pattern, "Введите дату родения", True, 1)
-
-    pattern = r"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$"
-    email = check_pattern(1, pattern, "Введите электронную почту", True, 1)
-    for el in phonebook:
-        if el["email"] == email:
-            print("Такой почтовый ящик уже существует в справочнике !")
-            print("Добавление данных отменено")
-            return
+    dateOfNote = date.today()
 
     record = dict()
-    record[fullname] = {'phones': phones}
-    if birthday != "":
-        record[fullname]['birthday'] = birthday
-    if email != "":
-        record[fullname]['email'] = email
+    record[title] = {'text': phones, 'date': dateOfNote}
     
-    return phonebook.update(record)
+    return notebook.update(record)
 
 
-def save(phonebook : dict) -> dict:
-    with open("contacts.json", "w", encoding="utf-8") as fh:
-        fh.write(json.dumps(phonebook, ensure_ascii=False))
-    return phonebook
+def save(notebook : dict) -> dict:
+    with open("notes.json", "w", encoding="utf-8") as fh:
+        fh.write(json.dumps(notebook, ensure_ascii=False))
+    return notebook
 
 
-def show(phonebook : dict) -> dict:
-    for key, values in phonebook.items():
+def show(notebook : dict) -> dict:
+    for key, values in notebook.items():
         print(key, values)
-    return phonebook
+    return notebook
 
 
-def search(phonebook : dict) -> dict:
-    for key, values in phonebook.items():
+def search(notebook : dict) -> dict:
+    for key, values in notebook.items():
         print(key, values)
-    return phonebook
+    return notebook
 
 
-def change(phonebook : dict) -> dict:
-    for key, values in phonebook.items():
+def change(notebook : dict) -> dict:
+    for key, values in notebook.items():
         print(key, values)
-    return phonebook
+    return notebook
 
 
-def delete(phonebook : dict) -> dict:
-    for key, values in phonebook.items():
+def delete(notebook : dict) -> dict:
+    for key, values in notebook.items():
         print(key, values)
-    return phonebook
+    return notebook
