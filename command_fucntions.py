6fccd7a54dee760c6b136f13644db314222422c1
i2gun <i2gun@bk.ru>
2024-02-17 16:01:30 +0600

    Resolving json.load issue

--- command_fucntions.py
+++ command_fucntions.py
@@ -1,48 +1,68 @@
-import json
+from json import loads, dumps
 from datetime import date
 
+
+def serialize_datetime(obj): 
+    if isinstance(obj, date): 
+        return obj.isoformat() 
+    
 
 def load() -> dict:
     json_records = {  1: {'title': "first record", 'text': "Hello World!", 'date': date.today()},
                       2: {'title': "some record",  'text': "some note",    'date': date.today()}, }
     try:
         with open("notes.json", "r", encoding="utf-8") as fh:
-            json_records = json.loads(fh.read())
+            # fh.write(dumps(notebook, ensure_ascii=False, default=serialize_datetime, ))
+            json_records = loads(fh.read().replace("; ", ", "))
+            print(json_records)
     except:
-        print("Загрузка заметок из файла")
-
+        print("Загрузка заметок из файла не удалась")
+    
     return json_records
 
 
 def add(notebook: dict) -> dict:
+    if not notebook:
+        notebook = dict()
+        id = 0
+    else:
+        id = notebook.list() + 1
+
     title = input("Введите заголовок заметки: ")
     for key, value in notebook:
-        if key.lower() == title.lower():
+        if value['title'].lower() == title.lower():
             print("Запись с таким заголовком уже имеется в заметках !")
             print("Добавление данных отменено")
             return
 
     textOfNote = input("Введите текст заметки: ")
-
-    record = dict()
-    record[title] = {'text': textOfNote, 'date': date.today()}
+    notebook[id] = {'title': title, 'text': textOfNote, 'date': date.today()}
     
-    return notebook.update(record)
+    return notebook
 
 
 def save(notebook):
-    with open("contacts.json", "w", encoding="utf-8") as fh:
-        fh.write(json.dumps(notebook, ensure_ascii=False))
+    with open("notes.json", "w", encoding="utf-8") as fh:
+        fh.write(dumps(notebook, ensure_ascii=False, default=serialize_datetime, separators=("; ", ": ")))
     return notebook
 
 
 def show(notebook):
+    if not notebook:
+        print("Записаня книга пуста")
+        return
+    
     for key, values in notebook.items():
         print(key, values)
+        
     return notebook
 
 
 def search(notebook):
+    if not notebook:
+        print("Записаня книга пуста")
+        return
+
     res_list = list()
 
     search_text = input("    | Введите заголовок заметки либо его часть: ").strip()
@@ -61,6 +81,10 @@ def search(notebook):
 
 
 def change(notebook):
+    if not notebook:
+        print("Записаня книга пуста")
+        return
+
     res_list = list()
     print("    |-----------------------")
     search_text = input("    | Введите заголовок заметки для внесения изменений: ").strip()
@@ -84,6 +108,10 @@ def change(notebook):
     
 
 def delete(notebook):
+    if not notebook:
+        print("Записаня книга пуста")
+        return
+
     res_list = list()
     print("    |-----------------------")
     search_text = input("    | Введите заголовок заметки для внесения изменений: ").strip()
